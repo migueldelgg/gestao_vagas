5349d7be86f3df4e2142655b7d6c82bcfe5e4753
4Miguel2 <delgdevoleper@gmail.com>
2024-04-30 20:46:34 -0300

    database connection and entity table creted

--- src/main/java/br/com/migueldelgado/gestao_vagas/modules/candidate/entity/CandidateEntity.java
+++ src/main/java/br/com/migueldelgado/gestao_vagas/modules/candidate/entity/CandidateEntity.java
@@ -1,27 +1,39 @@
 package br.com.migueldelgado.gestao_vagas.modules.candidate.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
-import java.rmi.server.UID;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Data
+@Entity(name = "candidate")
 public class CandidateEntity {
 
-    private UID id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
     private String name;
 
-    @Pattern(regexp = "^(?!\\s*$).+", message = "O campo [username] não deve conter espaço. ")
+    @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaço. ")
     private String username;
 
     @Email(message = "O campo [email] deve conter um email válido.")
     private String email;
 
-    @Length(min = 10, max = 100)
+    @Length(min = 10, max = 100, message = "A senha deve conter entre 10 e 100 caracteres.")
     private String password;
     private String description;
     private String curriculum;
 
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
