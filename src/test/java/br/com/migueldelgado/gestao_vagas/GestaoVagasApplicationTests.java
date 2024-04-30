18bb2b4da4a0a27198171aa677740fc89b9f01e7
4Miguel2 <delgdevoleper@gmail.com>
2024-04-30 10:09:14 -0300

    projeto criado

---
+++ src/test/java/br/com/migueldelgado/gestao_vagas/GestaoVagasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.migueldelgado.gestao_vagas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoVagasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
