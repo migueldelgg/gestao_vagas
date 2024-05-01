7013b62f01f5344006073476ae0141b8c11cba1c
4Miguel2 <delgdevoleper@gmail.com>
2024-05-01 13:35:10 -0300

    company controller

---
+++ src/main/java/br/com/migueldelgado/gestao_vagas/modules/company/controller/CompanyController.java
@@ -0,0 +1,31 @@
+package br.com.migueldelgado.gestao_vagas.modules.company.controller;
+
+import br.com.migueldelgado.gestao_vagas.modules.company.entity.CompanyEntity;
+import br.com.migueldelgado.gestao_vagas.modules.company.useCase.CreateCompanyUseCase;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/company")
+public class CompanyController {
+
+    @Autowired
+    CreateCompanyUseCase createCompanyUseCase;
+
+    @PostMapping("/")
+    public ResponseEntity<Object> create(@Valid @RequestBody CompanyEntity companyEntity){
+        try{
+            var result = this.createCompanyUseCase.execute(companyEntity);
+            return ResponseEntity.ok().body(result);
+        }catch (Exception e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+
+}
