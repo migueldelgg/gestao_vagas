0a2afdcd599c810592cd4d5eb9c5febb78a3ac4f
4Miguel2 <delgdevoleper@gmail.com>
2024-05-02 12:43:02 -0300

    UseCase : AuthCompany - token generated

---
+++ src/main/java/br/com/migueldelgado/gestao_vagas/modules/company/controller/AuthCompanyController.java
@@ -0,0 +1,34 @@
+package br.com.migueldelgado.gestao_vagas.modules.company.controller;
+
+import br.com.migueldelgado.gestao_vagas.modules.company.dto.AuthCompanyDTO;
+import br.com.migueldelgado.gestao_vagas.modules.company.useCase.AuthCompanyUseCase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.naming.AuthenticationException;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthCompanyController {
+
+    @Autowired
+    AuthCompanyUseCase authCompanyUseCase;
+
+    @PostMapping("/company")
+    public ResponseEntity<Object> create(@RequestBody AuthCompanyDTO authCompanyDTO) throws AuthenticationException {
+
+        try{
+            var result = this.authCompanyUseCase.execute(authCompanyDTO);
+            return ResponseEntity.ok().body(result);
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+        }
+
+    }
+}
--- src/main/java/br/com/migueldelgado/gestao_vagas/modules/company/useCase/CreateCompanyUseCase.java
+++ src/main/java/br/com/migueldelgado/gestao_vagas/modules/company/useCase/CreateCompanyUseCase.java
@@ -4,6 +4,7 @@ import br.com.migueldelgado.gestao_vagas.exceptions.UserFoundException;
 import br.com.migueldelgado.gestao_vagas.modules.company.entities.CompanyEntity;
 import br.com.migueldelgado.gestao_vagas.modules.company.repositories.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,6 +13,9 @@ public class CreateCompanyUseCase {
     @Autowired
     CompanyRepository companyRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public CompanyEntity execute(CompanyEntity companyEntity){
 
         this.companyRepository.findByUsernameOrEmail(companyEntity.getUsername(), companyEntity.getEmail())
@@ -19,6 +23,8 @@ public class CreateCompanyUseCase {
                     throw new UserFoundException();
                 });
 
+        var password = passwordEncoder.encode(companyEntity.getPassword());
+        companyEntity.setPassword(password);
         return this.companyRepository.save(companyEntity);
     }
 }
