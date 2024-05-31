998757188920a27585f21b7397f597b091165f3d
4Miguel2 <delgdevoleper@gmail.com>
2024-05-31 20:49:40 -0300

    Teste da execao de usuario nao encontrado.

---
+++ src/main/java/br/com/migueldelgado/gestao_vagas/modules/candidate/useCase/ApplyJobCandidateUseCase.java
@@ -0,0 +1,39 @@
+package br.com.migueldelgado.gestao_vagas.modules.candidate.useCase;
+
+import br.com.migueldelgado.gestao_vagas.exceptions.JobNotFoundException;
+import br.com.migueldelgado.gestao_vagas.exceptions.UserNotFoundException;
+import br.com.migueldelgado.gestao_vagas.modules.candidate.repositories.CandidateRepository;
+import br.com.migueldelgado.gestao_vagas.modules.company.repositories.JobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class ApplyJobCandidateUseCase {
+
+    @Autowired
+    private JobRepository jobRepository;
+
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+    //ID do candidato
+    // ID da vaga
+    public void execute(UUID idCandidate, UUID idJob) {
+
+        //Validar se o candidato existe
+        this.candidateRepository.findById(idCandidate).orElseThrow(() -> {
+            throw new UserNotFoundException();
+        });
+
+        // Validar se a vaga existe
+        this.jobRepository.findById(idJob).orElseThrow(() -> {
+            throw new JobNotFoundException();
+        });
+
+        //Candidato se inscrever na vaga
+
+    }
+
+}
---
+++ src/test/java/br/com/migueldelgado/gestao_vagas/modules/candidate/useCase/ApplyJobCandidateUseCaseTest.java
@@ -0,0 +1,37 @@
+package br.com.migueldelgado.gestao_vagas.modules.candidate.useCase;
+
+import br.com.migueldelgado.gestao_vagas.exceptions.JobNotFoundException;
+import br.com.migueldelgado.gestao_vagas.exceptions.UserNotFoundException;
+import br.com.migueldelgado.gestao_vagas.modules.candidate.repositories.CandidateRepository;
+import br.com.migueldelgado.gestao_vagas.modules.company.repositories.JobRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+public class ApplyJobCandidateUseCaseTest {
+
+    @InjectMocks
+    private ApplyJobCandidateUseCase applyJobCandidateUseCase;
+
+    @Mock
+    private CandidateRepository candidateRepository;
+
+    @Mock // identifica que a classe JobRepository é uma dependencia que esta presente dentro da classe ApplyJobCandidateUseCase
+    private JobRepository jobRepository;
+
+    @Test
+    @DisplayName("Should Not be able to apply job with candidate not found.")
+    public void should_not_be_able_to_apply_job_with_candidate_not_found(){
+        try{
+            applyJobCandidateUseCase.execute(null, null);
+        }catch (Exception e){
+            assertThat(e).isInstanceOf(UserNotFoundException.class);
+        }
+    }
+}
